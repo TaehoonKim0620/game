091fea18ac3365fdc8878edb247690ac8d9be3d5
Administrator <Administrator@i7E-13>
2025-01-15 10:59:27 +0900

    s

--- src/d0115/Variable.java
+++ src/d0115/Variable.java
@@ -1,5 +1,13 @@
 package d0115;
+// 변수는 영역 안이면 뭐든지 사용 가능
+// 클래스 안에서 변수를 선언하면 이름 : 멤버변수
+// 다른 영역 안에서 사용하면 멤버변수가 아님.
 
 public class Variable {
+	int i; // 멤버변수 o
+	public static void main(String[] args) {
+		int i; // 멤벼변수 x
+		System.out.println("adf"); // out 은 멤버변수
+	}
 
 }
--- src/d0115/Variable2.java
+++ src/d0115/Variable2.java
@@ -1,5 +1,10 @@
 package d0115;
-
+//main과 Variable2는 서로 다른 영역(중요)
 public class Variable2 {
-
+	int i; // 멤버변수
+	public static void main(String[] args) {
+		int i; // 변수를 사용하려면 선언과 초기화를 해야됨
+		// 멤버변수를 사용하려면 반드시 메모리를 생성해야됨
+		
+	}
 }
